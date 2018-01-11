a8b2c4ba388ed8014d8465eb48b6435739fb8e3a
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2018-01-11 09:58:26 +0100

    ignore more to force update

--- update/upgrade_db.sh
+++ update/upgrade_db.sh
@@ -132,7 +132,7 @@ pum baseline -p qwat_comp -t qwat_sys.info -d delta/ -b $VERSION
 printf "\n${BLUE}Test and upgrade qwat core${NC}\n\n"
 sleep 1s
 
-pum test-and-upgrade -pp qwat_prod -pt qwat_test -pc qwat_comp -t qwat_sys.info -d delta/ -f $TMPFILEDUMP -i columns constraints views sequences indexes triggers functions rules
+pum test-and-upgrade -x -pp qwat_prod -pt qwat_test -pc qwat_comp -t qwat_sys.info -d delta/ -f $TMPFILEDUMP -i tables columns constraints views sequences indexes triggers functions rules
 
 # applies local script to test
 

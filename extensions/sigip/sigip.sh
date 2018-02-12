be5a25ae3310c2cc04f450914cbcd97ab0c60fbb
Arnaud Poncet-Montanges <arnaudponcetmontanges@gmail.com>
2018-02-12 13:45:45 +0100

    Ajout de la table damage

--- extensions/sigip/sigip.sh
+++ extensions/sigip/sigip.sh
@@ -6,5 +6,6 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 psql -c "CREATE SCHEMA IF NOT EXISTS qwat_sigip;"
-#psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/sire_columns.sql
+psql -v ON_ERROR_STOP=1 -v SRID=$SRID -f ${DIR}/damage/damage.sql
+
 ${DIR}/insert_views.sh
